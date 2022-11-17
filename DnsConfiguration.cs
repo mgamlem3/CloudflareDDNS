b6fe0a5e2936977329318c236bb438676ca12d5b
Michael Gamlem III <mgamlem3@gmail.com>
2022-11-16 19:24:09 -0800

    Add zone and record identifiers

--- DnsConfiguration.cs
+++ DnsConfiguration.cs
@@ -2,9 +2,11 @@ namespace CloudflareDDNS;
 
 public class DnsConfiguration
 {
-	public DnsConfiguration(string type, string name, int ttl) => (Type, Name, TTL) = (type, name, ttl);
+	public DnsConfiguration(string type, string name, int ttl, string zoneIdentifier, string recordIdentifier) => (Type, Name, TTL, ZoneIdentifier, RecordIdentifier) = (type, name, ttl, zoneIdentifier, recordIdentifier);
 
 	public string Type { get; set; }
 	public string Name { get; set; }
 	public int TTL { get; set; }
+	public string ZoneIdentifier { get; set; }
+	public string RecordIdentifier { get; set; }
 }
