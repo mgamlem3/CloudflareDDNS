15a676ccced2a688647bfb3eb07f31126e23ff44
Michael Gamlem III <mgamlem3@gmail.com>
2022-11-18 17:35:34 -0800

    Add proxied variable

--- DnsConfiguration.cs
+++ DnsConfiguration.cs
@@ -2,11 +2,12 @@ namespace CloudflareDDNS;
 
 public class DnsConfiguration
 {
-	public DnsConfiguration(string type, string name, int ttl, string zoneIdentifier, string recordIdentifier) => (Type, Name, TTL, ZoneIdentifier, RecordIdentifier) = (type, name, ttl, zoneIdentifier, recordIdentifier);
+	public DnsConfiguration(string type, string name, int ttl, bool proxied, string zoneIdentifier, string recordIdentifier) => (Type, Name, TTL, Proxied, ZoneIdentifier, RecordIdentifier) = (type, name, ttl, proxied, zoneIdentifier, recordIdentifier);
 
 	public string Type { get; set; }
 	public string Name { get; set; }
 	public int TTL { get; set; }
+	public bool Proxied { get; set; }
 	public string ZoneIdentifier { get; set; }
 	public string RecordIdentifier { get; set; }
 }
